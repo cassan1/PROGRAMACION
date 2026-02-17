b6d4a10c91cabd32ac312c85543d52403dee10f8
Andres Lizarazo <fljj53144gmail.com>
2026-02-17 18:01:25 -0500

    Validacion de edad agregada

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Mi primer programa");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output:process.stdout
+});
+
+rl.question("¿cual es tu nombre? ", function(nombre){
+    rl.question("¿cual es tu edad?", function(edad){
+
+        edad = parseInt(edad);
+        if (isNaN(edad)){
+            console.log("por favor ingresa una edad valida.");
+        }else if(edad >=18){
+            console.log("Hola "+ nombre + ", eres mayor de edad.");
+        }else{
+            console.log("Hola "+ nombre +", eres menor de edad.");
+        }
+
+        rl.close();
+    });
+});
